bf88918b10705cee8d8a2d9ca75009b6b1e1c725
yerard97 <yerard2603@gmail.com>
2019-01-09 12:43:05 -0600

    Update login.php
    Actualización

--- php/login.php
+++ php/login.php
@@ -3,27 +3,30 @@ include_once "dbconfig.php";
 $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 $mysqli->set_charset("utf8");
 if (!$mysqli) die("No puede conectar a MySQL: " . mysql_error());
-//$user = $_POST['usuario']; 
-//$pass = $_POST['password'];
-$user = 'dante' ;
-$pass = '1234';
+$user = $_POST['usuario']; 
+$pass = $_POST['password'];
+//$user = 'dante' ;
+//$pass = '1234';
 $result = mysqli_query($mysqli, "select Password from usuariosistema where user='$user';");
 $row = mysqli_fetch_assoc($result);
-$hash = $row['Password'];
+$hash = password_hash($row['Password'],PASSWORD_DEFAULT);
 	if (password_verify($pass, $hash)) {	
+		session_start();
+        
+        $_SESSION['name'] = $user;					
 		
-		$_SESSION['loggedin'] = true;
-		$_SESSION['name'] = $row['Name'];
-		$_SESSION['start'] = time();
-		$_SESSION['expire'] = $_SESSION['start'] + (1 * 60) ;						
-		
-		echo "<div class='alert alert-success' role='alert'><strong>Welcome!</strong> $row[Name]			
-		<p><a href='edit-profile.php'>Edit Profile</a></p>	
-		<p><a href='logout.php'>Logout</a></p></div>";	
+		echo "<script type='text/javascript'>
+        alert('Bienvenido');
+        location.href ='../index.html#openmodal';
+        </script>
+        ";
 	
 	} else {
-		echo "<div class='alert alert-danger' role='alert'>Email or Password are incorrects!
-		<p><a href='login.html'><strong>Please try again!</strong></a></p></div>";			
+		echo "<script type='text/javascript'>
+        alert('Usuario o Contraseña Incorrectos');
+        location.href ='../index.html#openmodal';
+        </script>
+        ";			
 	}
-print_r($row);
+//print_r($row['Password']);
 ?>
