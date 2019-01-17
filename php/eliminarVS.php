41f44d7ab70dfe0ba7e7b33cf5a127803d0c9a60
yerard97 <yerard2603@gmail.com>
2019-01-17 09:04:52 -0600

    23

--- php/eliminarVS.php
+++ php/eliminarVS.php
@@ -9,8 +9,9 @@ $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 $mysqli->set_charset("utf8");
 if (!$mysqli) die("No puede conectar a MySQL: " . mysql_error());
 $mysqli->query("DELETE FROM detallevs WHERE NoPartida=$NoPartida && dvsvaleSalida=$var;");
+//echo $mysqli;
 if($mysqli){
-            echo "       <!doctype html>
+            echo "<!doctype html>
             <html>
             <head>
                 <script src='../lib/sweetalert.min.js'></script>
@@ -21,7 +22,7 @@ if($mysqli){
            	echo " 
             <script type='text/javascript'>
             swal('Eliminado Correctamente');
-            setTimeout(function(){ location.href ='../vale.php';}, 1000); 
+           // setTimeout(function(){ location.href ='../vale.php';}, 1000); 
             </script>";
 }
 ?>
